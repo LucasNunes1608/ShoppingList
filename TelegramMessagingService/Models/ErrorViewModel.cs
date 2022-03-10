749266b3c1667e9338df9470a51e9ff1cfdde610
Lucas Nunes <lucasmnunesk@gmail.com>
2022-03-10 01:39:50 -0300

    Removing files not used by sln

--- TelegramMessagingService/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TelegramMessagingService.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
